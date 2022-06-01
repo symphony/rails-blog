b64923da2a913ab3d8cd966994e8d24e7f19c90a
symphony <jciurc@gmail.com>
2022-06-01 14:20:26 -0600

    add (hardcoded) user authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  # authentication
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
